d46c29d196b3e30e4f72d0f20883495ec36706e2
DamienLeMalETPA <damienlemal123@gmail.com>
2023-08-05 00:47:24 +0200

    Back to normal + start of Qsearch

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -9,83 +9,97 @@ using static System.Formats.Asn1.AsnWriter;
 public class MyBot : IChessBot {
     int[] pieceValues = { 0, 100, 300, 310, 500, 900, 10000 };
     Move bestMove;
-    int maxDepth = 10;
-    Timer myTimer;
+    int maxDepth = 50;
+    int maxTime = 100;
+    int totalTime = 0;
     public Move Think (Board board, Timer timer)
     {
-        myTimer = timer;
+
         bestMove = Move.NullMove;
+        Move[] moves = board.GetLegalMoves();
+        
+
+        //stop if checkmate or one move possible
+        if (moves.Length == 1) return moves[0];
+        foreach (Move m in moves) {
+            board.MakeMove(m);
+            if (board.IsInCheckmate()) return m;
+            board.UndoMove(m);
+        }
 
         for (int i = 1; i <= maxDepth; i++) {
-            int score = EvaluateBoard(board, i, board.IsWhiteToMove ? 1 : -1, -999999, 999999, 0);
-            Console.WriteLine("Finnished depth " + i + " in " + timer.MillisecondsElapsedThisTurn);
-            if (timer.MillisecondsElapsedThisTurn >= 10000) {
-                //Console.WriteLine("Time's up");
+            bestMoveChanged = 0;
+            EvaluateBoard(board, timer, i, board.IsWhiteToMove ? 1 : -1, -999999, 999999, 0);
+            //Console.WriteLine(bestMoveChanged);
+            //Console.WriteLine(i - 1);
+            if (timer.MillisecondsElapsedThisTurn >= maxTime)
                 break;
-            }
         }
-
-        Console.WriteLine(timer.MillisecondsElapsedThisTurn + " ms");
-        if (bestMove == Move.NullMove) {
-            bestMove = board.GetLegalMoves()[0];
-            Console.WriteLine("illegal move");
-        }
-        return bestMove;
+        totalTime += timer.MillisecondsElapsedThisTurn;
+        int average = totalTime / (board.PlyCount/2+1);
+        Console.WriteLine("Average speed = " + average);
+        return transpositionsTable[board.ZobristKey & tpMask].BestMove;
     }
+    int bestMoveChanged = 0;
+    int EvaluateBoard (Board board, Timer timer, int depth, int color, int alpha, int beta, int ply = 0) {
 
-    int EvaluateBoard (Board board, int depth, int color, int alpha, int beta, int ply = 0) {
-
-
+        bool notRoot = ply > 0;
+        int maxScore = -999999;
+        int startingAlpha = alpha;
 
         ref Transposition transposition = ref transpositionsTable[board.ZobristKey & tpMask];
 
-        int maxScore = int.MinValue;
+        if (notRoot && board.IsRepeatedPosition()) return 0;
+
+        //Ordering moves
+
+        Move[] movesBestFirst = board.GetLegalMoves();
+        int[] movePriorityTable = new int[movesBestFirst.Length];
+
+        for (int i = 0; i < movesBestFirst.Length; i++) {
+            if (movesBestFirst[i] == transposition.BestMove)
+                movePriorityTable[i] = 1;
+            else
+                movePriorityTable[i] = 999999;
+        }
+
+        //Need work to avoid draw by repetition
+        if (notRoot && transposition.ZHash == board.ZobristKey && transposition.Depth > depth && (
+            transposition.Flag == 3 // exact score
+            || transposition.Flag == 2 && transposition.Eval >= beta // lower bound, fail high
+            || transposition.Flag == 1 && transposition.Eval <= alpha // upper bound, fail low
+        )) return transposition.Eval;
 
         if (depth == 0 || board.GetLegalMoves().Length == 0)
             return GetBoardScore(board, color);
 
-        if (transposition.ZHash == board.ZobristKey && transposition.Depth > depth) {
-            return transposition.Eval;
-        }
+        foreach (Move m in movesBestFirst) {
 
+            if (timer.MillisecondsElapsedThisTurn >= maxTime) return 999999;
 
-        foreach (Move m in board.GetLegalMoves()) {
             board.MakeMove(m);
-
-            ref Transposition t = ref transpositionsTable[board.ZobristKey & tpMask];
-
-            
-
-
-            int score = -EvaluateBoard(board,depth-1, -color, -alpha, -beta, ply+1);
-
-            if (depth > t.Depth) {
-                t.Depth = depth;
-                t.Eval = score;
-                t.ZHash = board.ZobristKey;
-                t.Flag = UPPERBOUND;
-            }
-
+            int score = -EvaluateBoard(board, timer, depth-1, -color, -beta, -alpha, ply+1);
+            board.UndoMove(m);
 
             if (score > maxScore) {
                 maxScore = score;
-                if (ply == 0)//only change if m is a legal move (undo move would result in current board)
-                    bestMove = m;
+                if (ply == 0 && bestMove != m)//only change if m is a legal move (undo move would result in current board)
+                    { bestMoveChanged++; bestMove = m;  }
             }
             alpha = Math.Max(alpha, maxScore);
             if (alpha >= beta) break;
-
-            board.UndoMove(m);
-
         }
+
+        int bound = maxScore >= beta ? 2 : maxScore > startingAlpha ? 3 : 1;
+        //Add to Transposition Table
+        transpositionsTable[board.ZobristKey & tpMask] = new Transposition(board.ZobristKey, maxScore, bestMove, (sbyte)depth, (sbyte)bound);
 
         return maxScore;
     }
 
     int GetBoardScore (Board b, int color) {
         if (b.IsInCheckmate()) return -999999;
-        if (b.IsDraw()) return 0;
-        if (b.IsRepeatedPosition()) return 0;
+        if (b.IsDraw() || b.IsRepeatedPosition()) return 0;
 
         int score = 0;
         for (int i = 0; ++i < 7;)
@@ -93,13 +107,11 @@ public class MyBot : IChessBot {
 
         score *= color;
 
-
         return score;
     }
 
 
     const ulong tpMask = 0x7FFFFF;
-    sbyte INVALID = 0, LOWERBOUND = 1, UPPERBOUND = 2, EXACT = 3;
     Transposition[] transpositionsTable = new Transposition[tpMask + 1];
 
 }
@@ -108,6 +120,13 @@ struct Transposition {
     public ulong ZHash;
     public int Eval;
     public Move BestMove;
-    public int Depth;
-    public sbyte Flag;
+    public sbyte Depth, Flag;
+
+    public Transposition(ulong zHash, int eval, Move bestMove, sbyte depth, sbyte flag) {
+        ZHash = zHash;
+        Eval = eval;
+        BestMove = bestMove;
+        Depth = depth;
+        Flag = flag;
+    }
 }
